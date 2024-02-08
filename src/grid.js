1a8d1223229cd8a7fc7e276b35f79cd74e650753
yah.cm@hotmail.com <yah.cm@hotmail.com>
2024-02-08 15:23:16 -0300

    Spotify v01

--- src/grid.js
+++ src/grid.js
@@ -18,10 +18,10 @@ const currentHour = new Date().getHours();
 // Forma mais simples
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
-    ? "Bom dia"
+    ? "Bom dia 🌞"
     : currentHour >= 12 && currentHour < 18
-    ? "Boa tarde"
-    : "Boa noite";
+    ? "Boa tarde 🌞"
+    : "Boa noite 🌙";
 
 greetingElement.textContent = greetingMessage;
 
